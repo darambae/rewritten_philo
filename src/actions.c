dc6cc32f3b04d42ddd73d1e651987ae8c455c3dd
darambae <64197421+jazzy1101@users.noreply.github.com>
2024-04-24 12:31:44 +0200

    .

--- src/actions.c
+++ src/actions.c
@@ -6,7 +6,7 @@
 /*   By: dabae <dabae@student.42perpignan.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 15:33:36 by dabae             #+#    #+#             */
-/*   Updated: 2024/04/23 16:20:55 by dabae            ###   ########.fr       */
+/*   Updated: 2024/04/24 11:21:34 by dabae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,37 +22,23 @@ void	print(t_philo *philo, char *msg)
 
 void	eat(t_philo *philo)
 {
-	pthread_mutex_lock(&philo->lock); // Lock the philosopher's lock to ensure atomic access to num_eat
-	if (philo->num_eat < philo->param->max_num_eat)
+	pthread_mutex_lock(philo->r_fork);
+	pthread_mutex_lock(philo->l_fork);
+	print(philo, "has taken both forks");
+	pthread_mutex_lock(&philo->lock);
+	philo->is_eating = true;
+	philo->num_eat++;
+	print(philo, "is eating");
+	philo->countdown_to_death = get_time() + philo->param->time_to_die;
+	ft_usleep(philo->param->time_to_eat);
+	philo->is_eating = false;
+	pthread_mutex_unlock(&philo->lock);
+	pthread_mutex_unlock(philo->r_fork);
+	pthread_mutex_unlock(philo->l_fork);
+	if (philo->num_eat == philo->param->max_num_eat)
 	{
-		pthread_mutex_lock(philo->l_fork);
-		print(philo, "has taken left fork");
-		pthread_mutex_lock(philo->r_fork);
-		print(philo, "has taken right fork");
-		philo->is_eating = true;
-		print(philo, "is eating");
-		philo->countdown_to_death = get_time() + philo->param->time_to_die;
-		ft_usleep(philo->param->time_to_eat);
-		pthread_mutex_unlock(philo->l_fork);
-		pthread_mutex_unlock(philo->r_fork);
-		philo->is_eating = false;
-		philo->num_eat++; // Increment the eat counter
-		if (philo->num_eat >= philo->param->max_num_eat) 
-		{
-			pthread_mutex_lock(&philo->param->stop_lock); // Lock the stop lock to ensure atomic access to stop
-			philo->param->num_full++; // Increment the number of philosophers that have eaten enough
-			pthread_mutex_unlock(&philo->param->stop_lock); // Unlock the stop lock
-			pthread_mutex_unlock(&philo->lock); // Unlock the philosopher's lock if they've finished eating
-			return ;
-		}
+		pthread_mutex_lock(&philo->param->stop_lock);
+		philo->param->num_full++;
+		pthread_mutex_unlock(&philo->param->stop_lock);
 	}
-	else
-	{
-		pthread_mutex_lock(&philo->param->stop_lock); // Lock the stop lock to ensure atomic access to stop
-		philo->param->num_full++; // Increment the number of philosophers that have eaten enough
-		pthread_mutex_unlock(&philo->param->stop_lock); // Lock the stop lock to ensure atomic access to stop		
-		pthread_mutex_unlock(&philo->lock); // Unlock the philosopher's lock
-		return;
-	}
-	pthread_mutex_unlock(&philo->lock); // Unlock the philosopher's lock
 }
--- src/life_cycle.c
+++ src/life_cycle.c
@@ -6,39 +6,76 @@
 /*   By: dabae <dabae@student.42perpignan.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 15:33:12 by dabae             #+#    #+#             */
-/*   Updated: 2024/04/23 16:33:50 by dabae            ###   ########.fr       */
+/*   Updated: 2024/04/24 12:29:00 by dabae            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
+
+void	stop_lock(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->param->stop_lock);
+	philo->param->stop = true;
+	pthread_mutex_unlock(&philo->param->stop_lock);
+}
 
 void	*set_stop(void *philo)
 {
 	t_philo	*phi;
 
 	phi = (t_philo *)philo;
-	while (1)
+	pthread_mutex_lock(&phi->param->stop_lock);
+	while (phi->param->stop == false && phi->param->num_full < phi->param->num_philo)
 	{
-		pthread_mutex_lock(&phi->lock);
-		if (phi->num_eat >= phi->param->max_num_eat)
-		{
-			pthread_mutex_unlock(&phi->lock);
-			break ;
-		}
-		if (phi->countdown_to_death < get_time())
-		{
-			phi->is_dead = true;
-			print(phi, "died");
-			pthread_mutex_unlock(&phi->lock);
-			break ;
-		}
+		pthread_mutex_unlock(&phi->param->stop_lock);
+		ft_usleep(1000);
+		pthread_mutex_lock(&phi->param->stop_lock);
+	}
+	if (phi->param->max_num_eat != -1 && phi->param->stop == false && phi->param->num_full >= phi->param->num_philo)
+	{
+		pthread_mutex_lock(&phi->param->print);
+		printf("Everyone has eaten as many times as %d\n", phi->param->max_num_eat);
+		pthread_mutex_unlock(&phi->param->print);
+		phi->param->stop = true;
+		pthread_mutex_unlock(&phi->param->stop_lock);
+	}
+	pthread_mutex_lock(&phi->lock);
+	if (phi->countdown_to_death <= get_time() && phi->is_eating == false)
+	{
+		stop_lock(phi);
+		phi->is_dead = true;
+		print(phi, "died");
 		pthread_mutex_unlock(&phi->lock);
 	}
-	pthread_mutex_lock(&phi->param->stop_lock);
-	phi->param->stop = true;
 	pthread_mutex_unlock(&phi->param->stop_lock);
 	return (NULL);
 }
+	// while (1)
+	// {
+	// 	pthread_mutex_lock(&phi->lock);
+	// 	if (phi->param->max_num_eat != -1 && \
+	// 		phi->param->num_full >= phi->param->num_philo)
+	// 	{
+	// 		pthread_mutex_lock(&phi->param->print);
+	// 		printf("All philosophers have eaten %d times\n", phi->param->max_num_eat);
+	// 		pthread_mutex_unlock(&phi->param->print);
+	// 		pthread_mutex_unlock(&phi->lock);
+	// 		stop_lock(phi);
+	// 		break ;
+	// 	}
+	// 	if (phi->countdown_to_death <= get_time() && phi->is_eating == false)
+	// 	{
+	// 		phi->is_dead = true;
+	// 		print(phi, "died");
+	// 		pthread_mutex_unlock(&phi->lock);
+	// 		stop_lock(phi);
+	// 		break ;
+	// 	}
+	// 	pthread_mutex_unlock(&phi->lock);
+	// 	//ft_usleep(1000);
+	// }
+// 	return (NULL);
+// }
 
 void	*routine(void *philo)
 {
@@ -47,7 +84,13 @@ void	*routine(void *philo)
 
 	phi = (t_philo *)philo;
 	pthread_create(&stop, NULL, &set_stop, phi);
-	while (phi->is_dead == false)
+	if (phi->param->num_philo == 1)
+	{
+		phi->is_dead = true;
+		pthread_join(stop, NULL);
+		return (NULL);
+	}
+	while (phi->is_dead == false && phi->param->stop == false && phi->param->num_full < phi->param->num_philo)
 	{
 		eat(phi);
 		print(phi, "is sleeping");
@@ -66,7 +109,7 @@ void	*monitor_stop(void *arg)
 	while (1)
 	{
 		pthread_mutex_lock(&param->stop_lock);
-		if (param->stop)
+		if (param->stop == true)
 		{
 			pthread_mutex_unlock(&param->stop_lock);
 			break ;
